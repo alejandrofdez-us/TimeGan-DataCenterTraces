f0e71b80049c10ffee2846dcd9671cc294135ded
alejandrofdez-us <afdez@us.es>
2022-10-16 10:54:33 +0200

    some refactoring and improvements

--- evaluation_metrics.py
+++ evaluation_metrics.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import random
+import re
 import statistics
 
 import numpy as np
@@ -33,16 +34,13 @@ def preprocess_dataset(ori_data, seq_len):
 
 
 def main (args):
-    directory_name = args.generated_data_dir + "/metrics/"
-    os.makedirs(directory_name, exist_ok=True)
-    metrics_list = [metric for metric in args.metrics.split(',')]
-
+    metrics_list, path_to_save_metrics, saved_experiments_parameters, saved_metrics = initialization(args)
 
     ori_data = np.loadtxt(args.ori_data_filename, delimiter=",", skiprows=1)
     ori_data[:, [1, 0]] = ori_data[:, [0, 1]] # timestamp como primera columna
     ori_data_histogram = torch.tensor(np.histogram(ori_data)[-1])
     if "tsne" in metrics_list or "pca" in metrics_list:
-        generate_visualization_figures(args, directory_name, metrics_list, ori_data)
+        generate_visualization_figures(args, path_to_save_metrics, metrics_list, ori_data)
 
     metrics_results = {}
     avg_results = {}
@@ -51,39 +49,69 @@ def main (args):
         metrics_results[metric] = []
         dtw_series = []
         for filename in os.listdir(args.generated_data_dir):
-            ori_data_sample_start = random.randrange(0, len(ori_data)-args.ori_sample_size)
-            ori_data_sample_end = ori_data_sample_start+args.ori_sample_size
-            ori_data_sample = ori_data[ori_data_sample_start:ori_data_sample_end]
+            ori_data_sample = get_ori_data_sample(args, ori_data)
             f = os.path.join(args.generated_data_dir, filename)
             if os.path.isfile(f): # checking if it is a file
                 generated_data_sample = np.loadtxt(f, delimiter=",")
-                generated_data_sample[:, [1, 0]] = generated_data_sample[:, [0, 1]] #timestamp como primera columna
+                #generated_data_sample[:, [1, 0]] = generated_data_sample[:, [0, 1]] #timestamp como primera columna
                 if metric == 'mmd': #mayor valor más distintas son
-                    mmd_result = mmd_rbf(X=ori_data_sample, Y=generated_data_sample)
-                    metrics_results[metric].append(mmd_result)
+                    mmd = mmd_rbf(X=ori_data_sample, Y=generated_data_sample)
+                    metrics_results[metric].append(mmd)
                 if metric == 'dtw': #mayor valor más distintas son
-                    res = dtw_ndim.distance(generated_data_sample, ori_data_sample)
-                    metrics_results[metric].append(res)
+                    dtw = dtw_ndim.distance(generated_data_sample, ori_data_sample)
+                    metrics_results[metric].append(dtw)
                 if metric == 'kl': #mayor valor peor
-                    dist = KLdivergence(ori_data, generated_data_sample)
-                    metrics_results[metric].append(dist)
+                    kl = KLdivergence(ori_data, generated_data_sample)
+                    metrics_results[metric].append(kl)
                 if metric == 'cc': #mayor valor peor
-                    ori_data_sample_numpy_pearson = np.corrcoef(ori_data_sample[:generated_data_sample.shape[0]])
-                    generated_data_sample_numpy_pearson = np.corrcoef(generated_data_sample)
-                    covariance_diff_matrix = ori_data_sample_numpy_pearson - generated_data_sample_numpy_pearson
-                    l1_norms_avg = np.mean([np.linalg.norm(row) for row in covariance_diff_matrix])
-                    metrics_results[metric].append(l1_norms_avg)
+                    cc = compute_cc(generated_data_sample, ori_data_sample)
+                    metrics_results[metric].append(cc)
 
 
     for metric, results in metrics_results.items():
         if metric == 'kl' or metric == 'dtw' or metric == 'mmd' or metric == 'cc':
             avg_results[metric] = statistics.mean(metrics_results[metric])
 
+    save_metrics(avg_results, metrics_results, path_to_save_metrics, saved_experiments_parameters, saved_metrics)
 
-    with open(directory_name + 'metrics.txt', 'w') as f:
-        f.write(repr(avg_results))
-        f.write("\n")
+
+def initialization(args):
+    path_to_save_metrics = args.generated_data_dir + "/metrics/"
+    f = open(args.generated_data_dir + '/../parameters.txt', 'r')
+    saved_experiments_parameters = f.readline()
+    f = open(args.generated_data_dir + '/../metrics.txt', 'r')
+    saved_metrics = f.readline()
+    args.seq_len = int(re.search("\Wseq_len[^:]*=\D*(\d+)", saved_experiments_parameters).group(1))
+    os.makedirs(path_to_save_metrics, exist_ok=True)
+    metrics_list = [metric for metric in args.metrics.split(',')]
+    return metrics_list, path_to_save_metrics, saved_experiments_parameters, saved_metrics
+
+
+def save_metrics(avg_results, metrics_results, path_to_save_metrics, saved_experiments_parameters, saved_metrics):
+    data_name = re.search("\Wdata_name[^:]*=\W*(\w+)", saved_experiments_parameters).group(1)
+    print("data_name", data_name)
+    iterations = re.search("\Witeration[^:]*=\D*(\d+)", saved_experiments_parameters).group(1)
+    seq_len = re.search("\Wseq_len[^:]*=\D*(\d+)", saved_experiments_parameters).group(1)
+    with open(path_to_save_metrics + 'metrics-'+data_name+'iterations-'+iterations+'-seq_len'+seq_len+'.txt', 'w') as f:
+        f.write(saved_experiments_parameters + '\n')
+        f.write(saved_metrics +'\n')
+        f.write(repr(avg_results) + '\n')
         f.write(repr(metrics_results))
+
+
+def compute_cc(generated_data_sample, ori_data_sample):
+    ori_data_sample_numpy_pearson = np.corrcoef(ori_data_sample[:generated_data_sample.shape[0]])
+    generated_data_sample_numpy_pearson = np.corrcoef(generated_data_sample)
+    covariance_diff_matrix = ori_data_sample_numpy_pearson - generated_data_sample_numpy_pearson
+    l1_norms_avg = np.mean([np.linalg.norm(row) for row in covariance_diff_matrix])
+    return l1_norms_avg
+
+
+def get_ori_data_sample(args, ori_data):
+    ori_data_sample_start = random.randrange(0, len(ori_data) - args.ori_sample_size)
+    ori_data_sample_end = ori_data_sample_start + args.ori_sample_size
+    ori_data_sample = ori_data[ori_data_sample_start:ori_data_sample_end]
+    return ori_data_sample
 
 
 def generate_visualization_figures(args, directory_name, metrics_list, ori_data):
@@ -128,7 +156,6 @@ if __name__ == '__main__':
         type=int)
 
     args = parser.parse_args()
-
     main(args)
 
 
