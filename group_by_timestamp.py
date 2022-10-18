8f987c6288989bd1c804b86f3f14bccc780ac041
alejandrofdez-us <afdez@us.es>
2022-10-18 16:05:19 +0200

    Group by para tratamiento de fichero completo de machine usage

--- group_by_timestamp.py
+++ group_by_timestamp.py
@@ -2,14 +2,23 @@ import pandas as pd
 import datetime
 
 
-start_date = datetime.datetime(2022, 1, 1)
+#start_date = datetime.datetime(2022, 1, 1)
 
 
-df = pd.read_csv('data/mu_day3.csv')
-df = df.drop('machine_id', axis=1)
-df['start_hour'] = df.apply(lambda row: (start_date + datetime.timedelta(seconds=row['time_stamp'])).hour, axis=1)
-df['start_minute'] = df.apply(lambda row: (start_date + datetime.timedelta(seconds=row['time_stamp'])).minute, axis=1)
-df['start_second'] = df.apply(lambda row: (start_date + datetime.timedelta(seconds=row['time_stamp'])).second, axis=1)
-df = df.groupby(['time_stamp']).mean()
+df = pd.read_csv('data/machine_usage_cut_days_3-4-5-6.csv')
+#df = df.drop(df.columns[[0,4,5]], axis=1) #drop machine_id and empty columns
+df = df.groupby(df.columns[0]).mean()
+df = df.drop(df.columns[0], axis=1) # falla!!
+df.to_csv('data/machine_usage_grouped_days_3-4-5-6.csv')
+#
+# for day in range (0,9):
+#     print ("Procesando día", day)
+#     df_day = df[(df[0] > 8640*day) & (df[0] < 8640*day+1)]
+#     df_day = df_day.groupby([0]).mean()
+#     df_day.to_csv('data/machine_usage_grouped_day_'+str(day)+'.csv')
+#     del df_day
 
-df.to_csv('data/mu_day3_grouped_hours.csv')
+#df['start_hour'] = df.apply(lambda row: (start_date + datetime.timedelta(seconds=row['time_stamp'])).hour, axis=1)
+#df['start_minute'] = df.apply(lambda row: (start_date + datetime.timedelta(seconds=row['time_stamp'])).minute, axis=1)
+#df['start_second'] = df.apply(lambda row: (start_date + datetime.timedelta(seconds=row['time_stamp'])).second, axis=1)
+
