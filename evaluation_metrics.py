8f8ba5f0fb2368b7cec3ab3676bff420db064b96
alejandrofdez-us <afdez@us.es>
2022-11-09 09:29:26 +0100

    Fixing dtw with one dimensional array

--- evaluation_metrics.py
+++ evaluation_metrics.py
@@ -76,7 +76,7 @@ def compute_metrics (args):
                 if metric == 'dtw': #mayor valor más distintas son
                     computed_metric = compute_dtw(generated_data_sample, ori_data_sample)
                     for column in range(generated_data_sample.shape[1]):
-                        metrics_results[metric+'-'+str(column)].append(compute_dtw(generated_data_sample[:,column], ori_data_sample[:,column]))
+                        metrics_results[metric+'-'+str(column)].append(compute_dtw(generated_data_sample[:,column].reshape(-1, 1), ori_data_sample[:,column].reshape(-1, 1)))
                 if metric == 'kl': #mayor valor peor
                     computed_metric = KLdivergence(ori_data, generated_data_sample)
                     for column in range(generated_data_sample.shape[1]):
